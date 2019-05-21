ea7f197ca679af5acfb98c2418bc992f9778492d
huyenpfiev <47497816+huyenpfiev@users.noreply.github.com>
2019-05-21 22:53:32 +0200

    Update colorDetection.py

--- Melanome_detection/colorDetection.py
+++ Melanome_detection/colorDetection.py
@@ -2,12 +2,47 @@ import numpy as np
 import argparse
 import cv2
 # load the image
-image = cv2.imread("IMGTRAIN/IMD8.jpg")
+image = cv2.imread("IMGTRAIN/IMD417/IMD417_Dermoscopic_Image/IMD417.bmp")
 
-hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 cv2.imshow("Original Image",image)
-# define the list of boundaries(white,black,red,blue,darkbrown,lightbrown)
+def threshFunction(image):
+    # Transform to gray colorspace and blur the image.
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (17, 17), 32)
+    # Perform Otsu threshold.
+    ret,thresh = cv2.threshold(blur,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    return thresh
+thresh=threshFunction(image)
+# Search for contours and iterate over contours. Make threshold for size to
+# eliminate others.
+im2, contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+#find max contourArea
+cnt = max(contours, key=cv2.contourArea)
+
+if len(cnt) > 4:
+        x,y,w,h = cv2.boundingRect(cnt)
+        
+img2 = image[y:y+h, x:x+w]
+thresh2=threshFunction(img2)
+
+img2[thresh2==0] = (0,153,0)
+cv2.imshow("Segmented Image",img2)
+cv2.waitKey(0);
+
+rgb = cv2.cvtColor(img2,cv2.COLOR_BGR2RGB)
+
+# define the list of boundaries(white,black,red,blue-gray,darkbrown,lightbrown)
 boundaries = [
+    ([205, 205, 205], [255, 255, 255]),
+    ([0, 0, 0], [50, 50, 50]),
+    ([150, 0, 0], [255, 50, 50]),
+    ([0, 100, 125], [150, 125, 150]),
+    ([50, 0, 0], [150, 100, 100]),
+    ([150, 50, 0], [200, 150, 100])
+ 
+]
+'''
+[
     ([0, 0, 200], [180, 255, 255]),
     ([0, 0, 0], [180, 255, 30]),
     ([0, 100, 100], [10, 255, 255]),
@@ -16,9 +51,10 @@ boundaries = [
     ([10, 100, 50], [20, 200, 200])
  
 ]
+'''
 # loop over the boundaries
 Values=[]
-Colors=['white','black','red','blue','darkbrown','lightbrown']
+Colors=['white','black','red','blue-gray','darkbrown','lightbrown']
 for (lower, upper) in boundaries:
     # create NumPy arrays from the boundaries
     lower = np.array(lower)
@@ -27,10 +63,10 @@ for (lower, upper) in boundaries:
     # find the colors within the specified boundaries and apply
     # the mask
     
-    mask = cv2.inRange(hsv, lower, upper)
-    output = cv2.bitwise_and(image, image, mask = mask)
+    mask = cv2.inRange(rgb, lower, upper)
+    output = cv2.bitwise_and(img2, img2, mask = mask)
     
-    if(cv2.countNonZero(mask)==0):
+    if(cv2.countNonZero(mask)<50):
                 Values.append(0)
     else:
                 Values.append(1)
@@ -46,4 +82,4 @@ for i in range(6):
     if(Values[i]==1):
         colorList.append(Colors[i])
 print("list of colors in the image:",colorList)
-        
+
